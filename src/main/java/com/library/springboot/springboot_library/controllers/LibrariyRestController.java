24e51df892ac58865a8a7decf6482dae30e782ec
Ismael <ism_fer_gar@hotmail.com>
2024-06-28 14:46:50 +0200

    Creating Rest Controller

---
+++ src/main/java/com/library/springboot/springboot_library/controllers/LibrariyRestController.java
@@ -0,0 +1,8 @@
+package com.library.springboot.springboot_library.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LibrariyRestController {
+
+}
