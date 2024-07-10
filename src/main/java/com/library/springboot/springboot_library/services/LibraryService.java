74f089ec3df076439bede9af6ca170d17afd9ed2
Ismael <ism_fer_gar@hotmail.com>
2024-07-10 18:05:40 +0200

    Updating Service Class with a bean

--- src/main/java/com/library/springboot/springboot_library/services/LibraryService.java
+++ src/main/java/com/library/springboot/springboot_library/services/LibraryService.java
@@ -3,8 +3,11 @@ package com.library.springboot.springboot_library.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
 import com.library.springboot.springboot_library.entities.Book;
 
+@Service
 public interface LibraryService {
 
     public List<Book> findAll();
