d90d5ebdd618e3debb433cc0c05ec2a69ab03e78
Ismael <ism_fer_gar@hotmail.com>
2024-07-18 13:30:42 +0200

    Function getBooks() is replaced by findAll() and now returns a Response Entity also created a log documentation

--- src/main/java/com/library/springboot/springboot_library/controllers/LibrariyRestController.java
+++ src/main/java/com/library/springboot/springboot_library/controllers/LibrariyRestController.java
@@ -6,23 +6,46 @@ import org.springframework.web.bind.annotation.RestController;
 import com.library.springboot.springboot_library.entities.Book;
 import com.library.springboot.springboot_library.services.LibraryService;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
-@RequestMapping("/app")
+@RequestMapping("/library")
+@Tag(name="Library",description="CRUD for the Library")
 public class LibrariyRestController {
 
     @Autowired
     LibraryService libraryService;
 
+    private static final Logger logger = LoggerFactory.getLogger(LibrariyRestController.class);
+
 
     @GetMapping("/books")
-    public List<Book> getBooks() {
-        return libraryService.findAll();
+    public ResponseEntity<List<Book>> findAll(){
+        logger.info("====================");
+        logger.info("Endpoint: /app/books");
+        logger.info("Showing All the books with a response entity (GET) request");
+        logger.info("====================");
+        List<Book> books = libraryService.findAll();
+        if(books.isEmpty()){
+            logger.info("==================");
+            logger.warn("The list of boooks is empty");
+            logger.info("==================");
+            return ResponseEntity.noContent().build();
+        }
+
+        logger.info("===================");
+        logger.info("The list of books is showed succesfully");
+        logger.info("===================");
+        return ResponseEntity.ok(books);
     }
     
 
