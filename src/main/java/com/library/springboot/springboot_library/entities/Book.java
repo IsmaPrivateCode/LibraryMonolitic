210f813f847b3a402d33c7b458cdbc6b508d25e7
Ismael <ism_fer_gar@hotmail.com>
2024-07-30 11:20:42 +0200

    Structure Book Entity

--- src/main/java/com/library/springboot/springboot_library/entities/Book.java
+++ src/main/java/com/library/springboot/springboot_library/entities/Book.java
@@ -1,7 +1,16 @@
 package com.library.springboot.springboot_library.entities;
 
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -9,12 +18,28 @@ import lombok.Data;
 public class Book {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotEmpty(message ="Title must not be empty")
+    @NotNull(message = "Title cannot be NULL")
     private String title;
-    private String authors;
+
+    @NotNull(message = "Author's cannot be empty")
+    @ManyToOne
+    private Author authors;
+    
+    @JsonProperty("description")
     private String description;
+
+    @NotNull(message="Category cannot be NULL")
     private String category;
+
+    @NotNull(message="Publisher cannot be NULL")
     private String publisher;
+
+    @NotNull(message="Price cannot be NULL")
     private double price;
 
+    
 }
