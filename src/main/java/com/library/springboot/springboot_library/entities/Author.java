debdaf0e593f4e097536d4aa104508c4c2ce652a
Ismael Fernandez <ismael.fernandez@nfq.com>
2024-08-05 09:06:48 +0200

    Fixing problems to run

--- src/main/java/com/library/springboot/springboot_library/entities/Author.java
+++ src/main/java/com/library/springboot/springboot_library/entities/Author.java
@@ -1,5 +1,14 @@
 package com.library.springboot.springboot_library.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Author {
+    @Id
+    private Long id;
+    private String name;
 
 }
