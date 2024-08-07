f0bdb428d85323d7e7458b869469587d1cec64b7
Ismael Fernandez <ismael.fernandez@nfq.com>
2024-08-07 09:56:08 +0200

    Making Author as an entity

--- src/main/java/com/library/springboot/springboot_library/entities/Author.java
+++ src/main/java/com/library/springboot/springboot_library/entities/Author.java
@@ -2,6 +2,7 @@ package com.library.springboot.springboot_library.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
@@ -9,6 +10,8 @@ import lombok.Data;
 public class Author {
     @Id
     private Long id;
+
+    @NotNull(message = "Name must not be null")
     private String name;
 
 }
